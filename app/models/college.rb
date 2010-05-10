7a3ccbe4687a74e87ad35b48f7a8c85e6838cd74
yas <ilyukevich.victor@gmail.com>
2010-05-10 23:23:35 +0300

    whitespaces

--- app/models/college.rb
+++ app/models/college.rb
@@ -2,6 +2,6 @@ class College < ActiveRecord::Base
   validates_presence_of :abbr, :name, :subdomain
   validates_uniqueness_of :abbr, :name, :subdomain, :case_sensitive => false
   has_many :users, :dependent => :nullify
-  
+
   has_many :newsitems, :through => :news_colleges
 end
