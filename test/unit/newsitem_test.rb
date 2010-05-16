5c61414e0f93eef8932daf6ee23ba94cb1bd23bd
yas <ilyukevich.victor@gmail.com>
2010-05-16 10:12:37 +0300

    написал unit тесты на создание новостей

--- test/unit/newsitem_test.rb
+++ test/unit/newsitem_test.rb
@@ -1,8 +1,12 @@
 require 'test_helper'
 
 class NewsitemTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  fixtures :newsitems
+
+  test "invalid with empty attributes" do
+    news = Newsitem.new
+    assert !news.valid?
+    assert news.errors.invalid?(:title)
+    assert news.errors.invalid?(:body)
   end
 end
