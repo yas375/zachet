afb4cf68478065b3b2399d1fb43687d52032889b
yas <ilyukevich.victor@gmail.com>
2010-05-29 13:49:10 +0300

    сделал, чтобы в зависимости от того вуз мы открываем или главную страницу, то подгружается ныжный шаблон

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
+    if params[:college_id] != 'helper'
+      @college = College.find(params[:college_id])
+      render :partial => 'index_college', :locals => {:college => @college}
+    end
   end
 end
