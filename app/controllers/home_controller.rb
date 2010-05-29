07ec77ea402f2aaa887624904c30c350392a7861
yas <ilyukevich.victor@gmail.com>
2010-05-29 14:37:43 +0300

    добавил layout для страниц универов

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     if params[:college_id] != 'helper'
       @college = College.find(params[:college_id])
-      render :partial => 'index_college', :locals => {:college => @college}
+      render :partial => 'index_college', :layout => 'college', :locals => {:college => @college}
     end
   end
 end
