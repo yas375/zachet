0d7fe46234827b744d665083e52d45d215951e91
yas <ilyukevich.victor@gmail.com>
2010-05-31 00:34:40 +0300

    сделал вывод последней новости. более развернуто. и чтобы в списках её не было.

--- app/models/newsitem.rb
+++ app/models/newsitem.rb
@@ -7,12 +7,24 @@ class Newsitem < ActiveRecord::Base
 
   # глобальные новоти сайта.
   named_scope :global, :joins => 'LEFT JOIN news_colleges nc ON nc.newsitem_id=newsitems.id',
-                       :conditions => 'nc.id IS NULL',
-                       :group => 'newsitems.id'
+                       :conditions => 'nc.id IS NULL'
 
   # новости всех универов, но не глобальные новоти сайта.
   named_scope :local_all, :joins => 'INNER JOIN news_colleges nc ON nc.newsitem_id=newsitems.id',
                           :group => 'newsitems.id'
+
+  # новости конкретного ВУЗа
+  named_scope :local, lambda {  |college|
+    { :joins => ['INNER JOIN news_colleges nc ON nc.newsitem_id=newsitems.id', 'INNER JOIN colleges c ON c.id=nc.college_id'],
+      :group => 'newsitems.id',
+      :conditions => ['c.subdomain=?', college] }
+    }
+
+  # чтобы выводить списки новостей на главных страницах
+  named_scope :without_first, :offset => 1
+
+  # ограницение для новостей на главных страницах
+  named_scope :news_on_main, :limit => 5
 
   def author
     user.login
