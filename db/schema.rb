db2bbdbdf85565c943502bf3c37c93f00942f7fa
Victor <ilyukevich.victor@gmail.com>
2010-06-27 15:01:21 +0300

    remove slugs model

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100520215516) do
+ActiveRecord::Schema.define(:version => 20100627112442) do
 
   create_table "colleges", :force => true do |t|
     t.string   "subdomain"
@@ -40,18 +40,6 @@ ActiveRecord::Schema.define(:version => 20100520215516) do
     t.datetime "updated_at"
     t.integer  "user_id"
   end
-
-  create_table "slugs", :force => true do |t|
-    t.string   "name"
-    t.integer  "sluggable_id"
-    t.integer  "sequence",                     :default => 1, :null => false
-    t.string   "sluggable_type", :limit => 40
-    t.string   "scope"
-    t.datetime "created_at"
-  end
-
-  add_index "slugs", ["name", "sluggable_type", "sequence", "scope"], :name => "index_slugs_on_n_s_s_and_s", :unique => true
-  add_index "slugs", ["sluggable_id"], :name => "index_slugs_on_sluggable_id"
 
   create_table "users", :force => true do |t|
     t.string   "login",                              :null => false
