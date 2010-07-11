8ac2fb2f0ef39bffa5953e6f6a823ae0a504fb67
yas <ilyukevich.victor@gmail.com>
2010-07-11 11:13:45 +0300

    remove parallel gem from environment

--- config/environment.rb
+++ config/environment.rb
@@ -42,8 +42,6 @@ Rails::Initializer.run do |config|
   config.gem 'searchlogic'
   config.gem 'simple-navigation'
   config.gem 'subdomain-fu'
-  config.gem 'parallel'
-  config.gem 'parallel_tests'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
