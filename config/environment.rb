a9a8684122dd80a4fb8e82560ae64702d6993808
yas <ilyukevich.victor@gmail.com>
2010-07-22 00:58:21 +0300

    replace prototype by jquery and jrails

--- config/environment.rb
+++ config/environment.rb
@@ -37,6 +37,7 @@ Rails::Initializer.run do |config|
   config.gem 'cells'
   config.gem 'friendly_id'
   config.gem 'formtastic'
+  config.gem 'jrails'
   config.gem 'haml'
   config.gem 'russian'
   config.gem 'searchlogic'
