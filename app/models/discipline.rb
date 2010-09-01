e354e0d59e19b39826ff3b79e1761d00a0cbdb53
yas <ilyukevich.victor@gmail.com>
2010-09-01 22:46:36 +0300

    replace discipline.name with display_name for preventing some errors

--- app/models/discipline.rb
+++ app/models/discipline.rb
@@ -9,7 +9,7 @@ class Discipline < ActiveRecord::Base
   validates_presence_of :name, :college
   validates_uniqueness_of :name, :scope => :college_id, :case_sensitive => false
 
-  def name
+  def display_name
     res = read_attribute('name')
     res << " (#{abbr})" if abbr
     res
