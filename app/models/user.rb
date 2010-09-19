2979f06c4d95f0b67b0d9de6d35d91c33e55e8de
yas <ilyukevich.victor@gmail.com>
2010-09-20 01:17:03 +0300

    add method name for user's model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ActiveRecord::Base
   acts_as_authentic
-  
+
   validates_uniqueness_of :login, :email
   belongs_to :college
-  
+
   def college_name
     college.abbr
+  end
+
+  def name
+    login
   end
 end
