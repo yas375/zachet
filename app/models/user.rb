939ea482fc7f78968b43478dae07a17d45f503d9
yas <ilyukevich.victor@gmail.com>
2010-09-22 21:50:07 +0300

    add validating of avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
+# -*- coding: utf-8 -*-
 class User < ActiveRecord::Base
   acts_as_authentic do |c|
     c.merge_validates_length_of_login_field_options :within => 2..100
   end
 
   has_attached_file :avatar, :styles => { :medium => "100x100>", :thumb => "40x40>" }
+  validates_attachment_content_type :avatar, :content_type => ['image/jpeg', 'image/jpg', 'image/pjpeg', 'image/gif', 'image/png', 'image/x-png', 'image/bmp'] # TODO check validation message
 
   symbolize :gender, :in => [:female, :male, :parquet], :allow_nil => true
 
