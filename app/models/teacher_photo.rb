393c990ab4ccd6f3c20805e99f20cd74957b2549
yas <ilyukevich.victor@gmail.com>
2010-09-26 13:38:58 +0300

    change teacher_photos pictures style

--- app/models/teacher_photo.rb
+++ app/models/teacher_photo.rb
@@ -1,7 +1,7 @@
 class TeacherPhoto < ActiveRecord::Base
   belongs_to :teacher
 
-  has_attached_file :picture, :styles => { :medium => "600x600", :thumb => "100x100#" }
+  has_attached_file :picture, :styles => { :medium => "600x600>", :thumb => "100x100>" }
 
   validates_attachment_presence :picture
   validates_attachment_size :picture, :less_than => 5.megabytes
