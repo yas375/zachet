cebebce81623b2ef67f65d5a808f9b5ccc7cf083
yas <ilyukevich.victor@gmail.com>
2010-10-06 19:23:52 +0300

    fix session store through subdomains

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,11 +2,12 @@
 
 # Your secret key for verifying cookie session data integrity.
 # If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
+# Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
   :key         => '_helper_session',
-  :secret      => 'e97d330c20c0f1898f8591018bc4986c9d46c9120637e1698ba58cb69ac3497eb633239ae6087e8dd7c7e576ae1aaff141b9debf0e49767b74c0f252a52337bc'
+  :secret      => 'e97d330c20c0f1898f8591018bc4986c9d46c9120637e1698ba58cb69ac3497eb633239ae6087e8dd7c7e576ae1aaff141b9debf0e49767b74c0f252a52337bc',
+  :domain      => ".#{AppConfig.host}"
 }
 
 # Use the database for sessions instead of the cookie-based default,
