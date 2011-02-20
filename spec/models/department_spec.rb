49c9545533d67a5e9addcde09c6c40bc67ba30ec
yas <ilyukevich.victor@gmail.com>
2011-02-20 13:05:37 +0200

    fix spec for department

--- spec/models/department_spec.rb
+++ spec/models/department_spec.rb
@@ -15,7 +15,7 @@ describe Department do
 
   context "validations" do
     it "should require faculty and name" do
-      should validate_presence_of :faculty, :name
+      should validate_presence_of :faculty_id, :name
     end
 
     it "should require unique name for each college" do
