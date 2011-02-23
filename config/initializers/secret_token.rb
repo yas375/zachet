65094dba352d8c7c1cab474aaac35912a5e7cd90
yas <ilyukevich.victor@gmail.com>
2011-02-23 18:16:56 +0200

    setup new rails add under old

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zachet::Application.config.secret_token = '18abc44ac02404c5e7b4b982df5b5602c4ab3d081980be8ba7555d11fca20f2289e5b38ba47dcf81caa4def99e11c6824383f4ba6e4f1b73c41d3923fe0fd3bd'
